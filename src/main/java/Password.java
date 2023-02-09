3f35819c8a563306ff8561a5f7193c5142c7a132
dsattler <01010174@edu.campus02.at>
2023-02-09 19:14:42 +0100

    Changed Pom

--- src/main/java/Password.java
+++ src/main/java/Password.java
@@ -1,2 +1,78 @@
-package PACKAGE_NAME;public class Password {
+import java.util.ArrayList;
+
+import static javax.print.attribute.standard.MediaSizeName.A;
+
+public class Password {
+
+    private String name;
+    private String encryption;
+    private int sign;
+
+    public static void main(String[] args) {
+        Password first = new Password("first", "encrypto", 4 );
+        System.out.println(first.toString());
+    }
+
+    public Password(String name, String encryption, int sign) {
+        this.name = name;
+        this.encryption = encryption;
+        setSign(sign);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEncryption() {
+        return encryption;
+    }
+
+    public void setEncryption(String encryption) {
+        this.encryption = encryption;
+    }
+
+    public int getSign() {
+        return sign;
+    }
+
+    public void setSign(int sign) {
+        if(sign < 0)
+        {
+            this.sign = 0;
+        }
+        else{
+            this.sign = sign;
+        }
+    }
+
+    public int  validatePasswordStrength(){
+
+        String password = "12334A%";
+
+        if(sign < 8)
+        {
+            return 0;
+        }
+        else if(sign >= 8){
+            return 1;
+        }
+        else
+        {
+            return 2;
+        }
+    }
+
+
+    @Override
+    public String toString() {
+        return "Password{" +
+                "name='" + name + '\'' +
+                ", encryption='" + encryption + '\'' +
+                ", sign=" + sign +
+                '}';
+    }
 }
--- src/test/java/PasswordTest.java
+++ src/test/java/PasswordTest.java
@@ -1,4 +1,25 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class PasswordTest {
-  
+
+    Password weak;
+
+
+    @BeforeEach
+    void setUp(){
+        weak = new Password("weak","onlysigns",7);
+    }
+
+    @Test
+    void validatePasswordStrength() {
+
+        int strength = 0;
+
+        Assertions.assertEquals(strength, weak.validatePasswordStrength());
+
+    }
 }
